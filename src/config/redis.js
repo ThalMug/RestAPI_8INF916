26dfd5a9251e8bd3eef57df23a233fff9a365487
thalmug <thaliameignan@gmail.com>
2024-05-01 14:24:59 -0400

    adding files

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,7 +1,7 @@
 ﻿const redis = require('redis');
 
 const redisClient = redis.createClient({
-    url: 'redis://redis:6379'
+    url: 'redis://127.0.0.1:6379'
 });
 redisClient.connect();
 
---
+++ src/ip_getter.js
@@ -0,0 +1,12 @@
+﻿const express = require('express');
+const app = express();
+
+app.get('/ip', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(ip);
+    res.status(200).json({ message: 'Success', ip_address: ip});
+});
+
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Server running on port 3001');
+});
