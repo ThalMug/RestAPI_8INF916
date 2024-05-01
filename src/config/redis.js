6a88fc21be1a30907db71c2a07b15fdf32f425a7
thalmug <thaliameignan@gmail.com>
2024-05-01 14:10:26 -0400

    :)

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,7 +1,7 @@
 ﻿const redis = require('redis');
 
 const redisClient = redis.createClient({
-    url: 'redis://172.19.0.2:6379'
+    url: 'redis://127.0.0.1:6379'
 });
 redisClient.connect();
 
--- src/controllers/serverController.js
+++ src/controllers/serverController.js
@@ -12,7 +12,8 @@ async function registerServer(req, res) {
             'INSERT INTO dedicated_server (server_id, ip_address) VALUES (uuid_generate_v4(), $1) RETURNING *',
             [ip_address]
         );
-        await redisClient.hSet(`server-info:${ip_address}`, 'lastUpdated', Date.now());
+        const serverSessionKey = `session:${ip_address}`;
+        const exists = await redisClient.exists(serverSessionKey);
         const token = jwt.sign(
             {
                 user_id: newServer.rows[0].server_id,
@@ -21,8 +22,15 @@ async function registerServer(req, res) {
             process.env.JWT_SECRET,
             { expiresIn: '1h' }
         );
+        if (!exists) {
+            await redisClient.sAdd(serverSessionKey, 'init');
 
-
+            console.log(`Server session created for IP: ${ip_address}`);
+        }
+        else {
+            console.log(`Session already exists for IP: ${ip_address}`);
+        }
+        
         res.status(200).json({ message: 'Success', token: token, server: newServer.rows[0] });
 
     } catch (err) {
@@ -33,9 +41,8 @@ async function registerServer(req, res) {
 
 async function addPlayer(req, res) {
     const { serverIp, playerUuid } = req.body;
-    const serverSessionKey = `server:1:${serverIp}`;
+    const serverSessionKey = `session:${serverIp}`;
     verifyJWT(req, res, async function () {
-        const userId = req.user_id;
         const role = req.role;
         if (role != "dedicated game server") {
             return res.status(403).json({ message: 'Forbidden' });
@@ -75,43 +82,42 @@ async function addPlayer(req, res) {
 }
 
 async function removePlayer(req, res) {
-    const { playerUuid } = req.body;
-    let serverIP = req.ip;
-
-    if (serverIP === '::1') {
-        serverIP = '127.0.0.1';
-    }
-
-    const serverSessionKey = `session:${serverIP}`;
+    const { serverIp, playerUuid } = req.body;
+    const serverSessionKey = `session:${serverIp}`;
     const playerKey = `player:${playerUuid}`;
     verifyJWT(req, res, async function () {
-        const userId = req.user_id;
         const role = req.role;
         if (role != "dedicated game server") {
             return res.status(403).json({ message: 'Forbidden' });
         }
-
         try {
-            await redisClient.sRem(serverSessionKey, playerUuid);
+            // Check if player exists in the database
+            const queryResult = await pool.query(
+                'SELECT rank, kda FROM users WHERE uuid = $1',
+                [playerUuid]
+            );
 
+            if (queryResult.rows.length === 0) {
+                res.status(404).json({ message: 'Player not found.' });
+                return;
+            }
+
+            // Remove player information from Redis
             await redisClient.del(playerKey);
 
+            // Remove player UUID from the server session set
+            await redisClient.sRem(serverSessionKey, playerUuid);
             console.log(`Player removed from session: ${playerUuid}`);
             res.status(200).json({ message: 'Player removed from server session.' });
         } catch (err) {
-            console.error('Redis error:', err.message);
+            console.error('Database or Redis error:', err.message);
             res.status(500).send('Server error');
         }
     });
 }
 
-
 async function getAllPlayerInfo(req, res) {
-    let serverIP = req.ip;
-
-    if (serverIP === '::1') {
-        serverIP = '127.0.0.1';
-    }
+    let {serverIP} = req.body;
 
     const serverSessionKey = `session:${serverIP}`;
     verifyJWT(req, res, async function () {
